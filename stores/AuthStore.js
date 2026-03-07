b7c2600159e8ebfe356b504e7351146ae8907a36
Metano Chavana <metanochava@gmail.com>
2026-03-07 23:05:39 +0100

    release: v0.0.13 - 1

--- stores/AuthStore.js
+++ stores/AuthStore.js
@@ -534,16 +534,16 @@ export const UserStore = defineStore("user", {
         const tipoEntidadeStore = TipoEntidadeStore()
         const rsp = await HTTPAuth.get(url({ type: 'u', url: 'api/django_resaas/entidades/' + Entidade?.id + '/themeGet/', params: { } }))
           .then(res => {
-            setStorage('c', 'entidadeTheme', JSON.stringify(res.data), 365)
             this.Theme = res.data || tipoEntidadeStore.Theme
+            setStorage('c', 'entidadeTheme', JSON.stringify(this.Theme), 365)
           }).catch(err => {
             console.log(err)
           })
 
           const lay = await HTTPAuth.get(url({ type: 'u', url: 'api/django_resaas/entidades/' + Entidade?.id + '/layoutsettingsGet/', params: { } }))
           .then(res => {
-            setStorage('c', 'entidadeThemeLayoutsettings', JSON.stringify(res.data), 365)
             this.LayoutSettings = res.data || tipoEntidadeStore.LayoutSettings
+            setStorage('c', 'entidadeThemeLayoutsettings', JSON.stringify(this.LayoutSettings), 365)
           }).catch(err => {
             console.log(err)
           })
