505327be249f62ef27435cafc282e7dccbdb3628
Metano Chavana <metanochava@gmail.com>
2026-03-16 08:53:36 +0100

    release: v0.0.42 - as

---
+++ components/engine/SelectComponent.vue
@@ -0,0 +1,53 @@
+
+
+<template>
+
+  <q-select
+    v-bind="attrs"
+
+    :dense="attrs.dense ?? layout.input_dense"
+
+    :outlined="attrs.outlined ?? layout.input_style === 'outlined'"
+    :filled="attrs.filled ?? layout.input_style === 'filled'"
+    :standout="attrs.standout ?? layout.input_style === 'standout'"
+
+    :class="attrs.class"
+    :style="{
+      borderRadius: layout.square
+        ? '0px'
+        : layout.rounded
+        ? '16px'
+        : '4px'
+    }"
+  >
+    <slot/>
+  </q-select>
+
+</template>
+
+<script>
+
+import { defineComponent, computed, useAttrs } from 'vue'
+import { UserStore } from "./../../stores/AuthStore"
+
+export default defineComponent({
+
+  name:"s-input",
+  inheritAttrs:false,
+
+  setup(){
+
+    const attrs = useAttrs()
+    const User = UserStore()
+
+    const layout = computed(()=>User.ps?.layout || {})
+
+    return{
+      attrs,
+      layout
+    }
+
+  }
+
+})
+</script>
