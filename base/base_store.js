0a5eafc55a8a2a04ad2f2787c5eae75c16e87e96
Metano Chavana <metanochava@gmail.com>
2026-04-28 22:33:13 +0200

    release: v0.0.417 - assa

--- base/base_store.js
+++ base/base_store.js
@@ -1,9 +1,6 @@
 import { createBaseStore } from '../base/base_store'
 import { HTTPAuth, HTTPClient, url } from '../boot/api'
-import { useSucursalStore } from './SucursalStore'
-import { useUserStore } from './UserStore'
-import { perfilSplint, tdc } from '../boot/base'
-import { getStorage, setStorage } from '../boot/storage'
+
 
 export const usePermissionStore = createBaseStore(
   'permission',
