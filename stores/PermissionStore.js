a1099f287a29e32c775dd47c9a6e71d54bd971a4
Metano Chavana <metanochava@gmail.com>
2026-05-01 15:43:46 +0200

    release: v0.0.436 - as

--- stores/PermissionStore.js
+++ stores/PermissionStore.js
@@ -120,7 +120,7 @@ export const usePermissionStore = createBaseStore(
         try {
           if (!exists) {
             await HTTPClient.post(
-              url({ type: 'u', url: `permissions/${permission.id}/addToGroup/` }),
+              url({ type: 'u', url: `api/auth/permissions/${permission.id}/addToGroup/` }),
               { id: this.group.id }
             )
 
@@ -128,7 +128,7 @@ export const usePermissionStore = createBaseStore(
 
           } else {
             await HTTPClient.post(
-              url({ type: 'u', url: `permissions/${permission.id}/removeFromGroup/` }),
+              url({ type: 'u', url: `api/auth/permissions/${permission.id}/removeFromGroup/` }),
               { id: this.group.id }
             )
 
