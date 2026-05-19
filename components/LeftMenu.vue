a374f138a1151bf57ee10d61f0acb31204525ef4
Metano Chavana <metanochava@gmail.com>
2026-05-19 20:04:08 +0200

    release: v0.0.607 - jk

--- components/LeftMenu.vue
+++ components/LeftMenu.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <s-card  square flat :class="$q.dark.isActive ? 'bg-dark text-white fixed-top  q-pa-0  header-fixed' : 'bg-saass text-white   q-pa-0 fixed-top header-fixed' ">
+  <s-card  square flat :class="$q.dark.isActive ? 'bg-dark  fixed-top  q-pa-0  header-fixed' : 'bg-trasparent  q-pa-0 fixed-top header-fixed' ">
     <div>
       <div class="text-center text-h6 q-pt-lg">
         {{ User?.username }}
