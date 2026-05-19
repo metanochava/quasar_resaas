57f86d9bf5c466e963d129617b1be4e761def02d
Metano Chavana <metanochava@gmail.com>
2026-05-19 23:47:34 +0200

    release: v0.0.632 - gh

--- components/LeftMenuSegundo.vue
+++ components/LeftMenuSegundo.vue
@@ -1,6 +1,6 @@
 <template>
   <div >
-    <search-menu :size="'298'" class="search-fixed" style="padding: 0px; margin-bottom: 20px;"/>
+    <search-menu :size="'250'" class="search-fixed" style="padding: 0px; margin-bottom: 20px;"/>
     <s-card
       square
       flat
