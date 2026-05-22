cf95bb9adc29dd3e24b6849c13d14832a760a9cb
Metano Chavana <metanochava@gmail.com>
2026-05-22 22:03:49 +0200

    release: v0.0.699 - dfdf

--- components/LeftMenuSegundo.vue
+++ components/LeftMenuSegundo.vue
@@ -1,16 +1,36 @@
 <template>
-  <div class="bg-grey" style="width: 300px; margin-top: 100px;  margin-left: -2px;  height: calc(100vh - 205px);">
+  <div
+    class="bg-grey overflow-hidden"
+    style="
+      width: 300px;
+      margin-top: 100px;
+      margin-left: -2px;
+      height: calc(100vh - 205px);
+    "
+  >
+
     <q-scroll-area
       class="fit"
       :thumb-style="thumbStyle"
       :bar-style="barStyle"
-       style="height: calc(100vh - 205px);"
-    > 
+      style="
+        height: calc(100vh - 205px);
+        width: 100%;
+      "
+    >
 
-      <q-list class="rounded-borders" style="width: 290px; ">
+      <q-list
+        class="rounded-borders"
+        style="
+          width: 100%;
+          overflow-x: hidden;
+        "
+      >
+
         <q-expansion-item
           v-for="App in User.Menus"
           :key="App"
+          class="full-width"
 
           :class="
             $q.dark.isActive
@@ -41,52 +61,19 @@
         </q-expansion-item>
 
       </q-list>
+
     </q-scroll-area>
 
   </div>
 </template>
-<script >
 
-
-import { defineComponent, h } from 'vue'
-import { tdc, toPlural} from '../boot/base'
-import { useUserStore } from '../stores/UserStore'
-import { useEntityTypeStore } from '../stores/EntityTypeStore'
-import SubMenu from './SubMenu.vue'
-
-
-export default defineComponent({
-  name: 'LeftMenuSegundo',
-
-  components: {
-    SubMenu
-  },
-  setup () {
-    const EntityType = useEntityTypeStore()
-    const  User = useUserStore()
-    return {
-      EntityType,
-      User,
-      tdc,
-      toPlural
-    }
-  },
-  data () {
-    return {
-
-    }
-  },
-  computed: {
-
-  },
-  watch: {
-
-  },
-  mounted () {
-
-  },
-  methods: {
-
-  }
-})
-</script>
+<style >
+.q-expansion-item,
+.q-item,
+.q-item__label {
+  max-width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+</style>
