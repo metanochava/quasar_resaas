b9ced6e0d432dd45541e7a72cc94bb9664cf1e1d
Metano Chavana <metanochava@gmail.com>
2026-06-16 16:35:17 +0200

    release: v0.0.792 - sds

--- components/engine/FormComponent.vue
+++ components/engine/FormComponent.vue
@@ -267,7 +267,7 @@ defineExpose({
           :key="f.name"
           class="col-md-4 col-sm-6 col-xs-12"
         >
-        <pre>{{ field.component }}</pre>
+        <pre>{{ f.component }}</pre>
           <component
             :is="f.component"
             v-model="form[f.name]"
@@ -288,7 +288,7 @@ defineExpose({
           :key="f.name"
           class="col-md-4 col-sm-6 col-xs-12"
         >
-        <pre>{{ field.component }}</pre>
+        <pre>{{ f.component }}</pre>
           <component
             :is="f.component"
             v-model="form[f.name]"
@@ -320,7 +320,7 @@ defineExpose({
           </template> 
 
           <!-- INPUT -->
-           <pre>{{ field.component }}</pre>
+           <pre>{{ f.component }}</pre>
           <component
             :is=" f.component"
             v-model="form[f.name]"
