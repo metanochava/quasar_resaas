d61c24df95de72698e90c9d76497fa1ceaf479e2
Metano Chavana <metanochava@gmail.com>
2026-06-26 14:15:51 +0200

    release: v0.0.853 - [A[A

--- base/base_store.js
+++ base/base_store.js
@@ -385,7 +385,7 @@ export function createBaseStore(name, config, extend = {}) {
           ? await this.update()
           : await this.create()
 
-          console.log("post": data)
+          console.log("post", data)
         return data
       },
 
