8514957be9ee782c584e068658c40b554638a037
Metano Chavana <metanochava@gmail.com>
2026-08-13 14:12:35 +0200

    release: v0.0.892 - jh

--- router/authRoutes.js
+++ router/authRoutes.js
@@ -11,7 +11,6 @@ export const authRoutes = [
   },
   {
     path: '/',
-    // component: () => import('../layouts/AuthLayout.vue'),
     component: () => import('../layouts/MainLayout.vue'),
     children: [
       { path: '/welcome', component: () => import('./../pages/WelcomeGuestPage.vue'), name: 'welcome' }
