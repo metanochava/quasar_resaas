d5af6df169c75799769dc6c9a18e7a0fe910c05d
Metano Chavana <metanochava@gmail.com>
2026-08-15 21:52:04 +0200

    release: v0.0.959 - jh

--- components/auto/AutoCrud.vue
+++ components/auto/AutoCrud.vue
@@ -130,13 +130,10 @@ const columns = computed(() => {
   }))
 
   const columns = [ 
-    {  name: '__actions',label: 'Acções', field: '__lactions', sortable: false, align: 'left', },
+    {  name: '__actions',label: 'Acções', field: '__lactions', sortable: false, align: 'left',  headerClasses: 'text-left' },
     ...base, 
-    { name: '__actions', label: 'Acções', field: '__ractions', sortable: false , align: 'right',  }
+    { name: '__actions', label: 'Acções', field: '__ractions', sortable: false , align: 'right',  headerClasses: 'text-right' }
   ]
-
-  columns[0].headerClasses = 'text-left'
-  columns[0].align = 'left'
     
   return columns
 })
