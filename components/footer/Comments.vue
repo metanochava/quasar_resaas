e2bea7dd3328b340c98b37d6343353d959ef22cb
Metano Chavana <metanochava@gmail.com>
2026-08-26 15:00:06 +0200

    release: v0.0.1091 - ds

--- components/footer/Comments.vue
+++ components/footer/Comments.vue
@@ -12,19 +12,17 @@
 
           <q-card-section >
             <s-input outlined
-                v-model="comment_text"
-                class=""
-                dense
-                :placeholder="tdc('Have feedback or suggestions? We would be happy to hear from you. Please do not include passwords, sensitive personal data, or confidential organizational information. <br>Need assistance? Visit the Help Center or contact your organization\'s support team.').replaceAll('<br>', '\n\n')"
-                type="textarea"
-                :rules="[ val => val && val.length > 0 || tdc('This field is required.')]"
-                />
+              v-model="comment_text"
+              class=""
+              dense
+              :placeholder="tdc('Have feedback or suggestions? We would be happy to hear from you. Please do not include passwords, sensitive personal data, or confidential organizational information. <br>Need assistance? Visit the Help Center or contact your organization\'s support team.').replaceAll('<br>', '\n\n')"
+              type="textarea"
+              :rules="[ val => val && val.length > 0 || tdc('This field is required.')]"
+            />
           </q-card-section>
 
           <q-card-section >
-            <div
-              v-html="tdc('Some account, entity, branch, and system information may be collected and processed to provide support, diagnose and resolve technical issues, maintain security, and improve the quality of our services. This information may be shared with authorized administrators or service providers when necessary, in accordance with the applicable Privacy Policy and Terms of Service. <br><br> We may contact you by email or other authorized communication channels if additional information is required or to provide updates regarding your request. For privacy, data protection, or legal matters, please contact your organization or the appropriate system administrator.')"
-            ></div>
+            <label v-html="tdc('Some account, entity, branch, and system information may be collected and processed to provide support, diagnose and resolve technical issues, maintain security, and improve the quality of our services. This information may be shared with authorized administrators or service providers when necessary, in accordance with the applicable Privacy Policy and Terms of Service. <br><br> We may contact you by email or other authorized communication channels if additional information is required or to provide updates regarding your request. For privacy, data protection, or legal matters, please contact your organization or the appropriate system administrator.')"/>
           </q-card-section>
           <q-separator />
 
