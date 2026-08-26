d10c029013e0206bd351303f6d66f833d1b9e771
Metano Chavana <metanochava@gmail.com>
2026-08-26 15:39:35 +0200

    release: v0.0.1098 - sd

--- components/footer/Comments.vue
+++ components/footer/Comments.vue
@@ -24,8 +24,7 @@
           outlined
           dense
           type="textarea"
-          :placeholder="
-            tdc('Have feedback or suggestions? We would be happy to hear from you. Please do not include passwords, sensitive personal data, or confidential organizational information. <br>Need assistance? Visit the Help Center or contact your organization's support team.').replaceAll('<br>', '\n\n')"
+          :placeholder=" tdc(`Have feedback or suggestions? We would be happy to hear from you. Please do not include passwords, sensitive personal data, or confidential organizational information. <br>Need assistance? Visit the Help Center or contact your organization's support team.`).replaceAll('<br>', '\n\n')"
           :rules="[
             val =>
               (val && val.length > 0) ||
